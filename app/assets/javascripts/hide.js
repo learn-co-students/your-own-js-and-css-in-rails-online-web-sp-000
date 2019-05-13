169a5e91e03963120fb577cb7696b06babcd5174
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-05-13 16:48:55 -0400

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,11 +1,4 @@
-// function hideWhenClicked () {
-//     document.getElementById('hide_this').style.visibility = "hidden";
+function hideWhenClicked () {
+    document.getElementById('hide_this').style.visibility = "hidden";
    
-// }
-function hideWhenClicked(e){
-	let $target = $(e.target);
-	$target.hide();
-
 }
-
-$('#hide_this').click(hideWhenClicked)
