1d6a4ecfbcda559966e681686bf8e496f953315e
jonmackeyparty <jon@jonmackey.party>
2019-06-04 15:08:51 -0400

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,6 +1,5 @@
+document.getElementById("hide_this").addEventListener("click", hideWhenClicked);
+
 function hideWhenClicked() {
-  $("#hide_this").onclick = function() {
-    this.style.visibility = "hidden";
-    }.bind(this)
-  }
+  this.style.visibility = "hidden";
 }
